b9bb4798dd88af8a717f87f33d7a6e2516957716
Caleb Logemann <caleb.logemann@gmail.com>
2016-09-11 23:44:41 -0500

    Wrote up the LP for problem 3 and made a untested sage script to solve that problem

---
+++ Homework/02_3.py
@@ -0,0 +1,12 @@
+# 
+
+p = MixedIntegerLinearProgram(maximization=False)
+c = p.newvariable(integer=True, nonnegative=True)
+p.set_objective(c[1] + c[2] + c[3] + c[4] + c[5] + c[6] + c[7] + c[8] + c[9] + c[10] + c[11] + c[12])
+p.add_constraint(2*c[1] + c[2] + c[3] + c[4] >= 97)
+p.add_constraint(c[2] + 2*c[5] + c[6] + c[7] + c[8] >= 610)
+p.add_constraint(c[3] + 2*c[6] + c[7] + 3*c[9] + 2*c[10] + c[11] >= 395)
+p.add_constraint(c[2] + c[3] + 3*c[4] + 2*c[5] + 2*c[7] + 4*c[8] + 2*c[10] + 4*c[11] + 7*c[12] >= 211)
+p.show()
+p.solve()
+
